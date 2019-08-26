1e9f69aac5b3440409f50d4b22c91c7aa7a88ab4
Joy <greeniejoy@gmail.com>
2019-08-26 14:20:07 -0500

    made database

--- schema.sql
+++ schema.sql
@@ -1,16 +1,18 @@
-DROP DATABASE IF EXISTS test;
+DROP DATABASE IF EXISTS english_japanese_dictionary;
 
-CREATE DATABASE test;
+CREATE DATABASE english_japanese_dictionary;
 
-USE test;
+USE english_japanese_dictionary;
 
-CREATE TABLE items (
+CREATE TABLE english_japanese_count (
   id int NOT NULL AUTO_INCREMENT,
-  quantity integer NOT NULL,
-  description varchar(50) NOT NULL,
+  english varchar(50) NOT NULL,
+  hirigana varchar(50) NOT NULL,
+  kanji varchar(50) NOT NULL,
   PRIMARY KEY (ID)
 );
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    //mysql -u root -p
+ mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
