2e3240dd3180442f900a36fdf9ac68a07e19a12f
Joy <greeniejoy@gmail.com>
2019-08-28 09:24:50 -0500

    Render the kanji word and hirgana on page after searching

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,21 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+//  import ListWord from './components/ListWord.jsx';
+
 // import List from './components/List.jsx';
 // import WordList from './components/WordList.jsx';
 import Search from './components/Search.jsx';
+import ListWord from './components/ListWord.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       word: '',
+      hiragana: '',
+      kanji: ''
     };
     // this.getwords = this.getwords.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     // this.handleClick = this.handleClick.bind(this);
   }
 
+
+  
   handleSearch(word) {
     console.log("testing");
     console.log('word was searched:', word);
@@ -24,21 +31,25 @@ class App extends React.Component {
     axios.post('/word', { english: word })
       .then((response) => {
         // response.send('words have saved to database');
-        this.state.word = response;
+        const kanji = response.data.data[0].slug;
+        const hiragana = response.data.data[0].japanese[0].reading;
+
+        this.setState({
+          kanji: kanji,
+          hiragana: hiragana,
+          word: word,
+        });
         console.log('response from front', response.data);
       })
       .catch((error) => {
-        // .status(400).send('unable to save to database');
         console.log(error);
       }); 
 
       axios.get('/top')
       .then((response) => {
-        // response.send('words have saved to database');
         console.log('response from front', response.data);
       })
       .catch((error) => {
-        // .status(400).send('unable to save to database');
         console.log(error);
       });
 
@@ -50,18 +61,25 @@ class App extends React.Component {
   // }
 
   render() {
-    const { word } = this.state;
+    const { word, hiragana, kanji } = this.state;
     // const { english } = this.state;
 
     return (
       <div>
-       <h1>Jisho Dictionary</h1>
-       <h1>日本語の事象</h1>
+        <h1>Japanese Dictionary</h1>
+        <h1>日本語の事象</h1>
        
+       {/* <ListWord /> */}
+       {/* <English english={this.state.word} />
+       <Hiragana hiragana={this.state.hiragana}/>
+       <Kanji kanji={this.state.kanji}/> */}
       {/* <WordList repos={ this.state.word } />
       <List words={ this.state.word } /> */}
-      <Search onSearch={this.handleSearch} />
-     </div>
+        <Search onSearch={this.handleSearch} />
+        <h1>{word}</h1>
+        <h1>{kanji}</h1>
+        <h1>{hiragana}</h1>
+      </div>
     )
   }
 }
